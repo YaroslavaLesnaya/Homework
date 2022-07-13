6070fafb76de87945482f87f75b213cd912b2096
Yaroslava <yaroslavalesnaya@gmail.com>
2022-07-14 02:41:48 +0300

    change_HW2_HW3

--- hw_2/main.js
+++ hw_2/main.js
@@ -1,36 +1,23 @@
-// let number1 = parseInt(prompt('введите n'),10);
-// let number2 = parseInt(prompt('введите m'),10);
-
-let number1;
-let number2;
+let number1 = 0;
+let number2 = 0;
 
 do {
-  let checkNumber = parseInt(prompt('введите n'),10);
-  number1 = checkNumber;
-} while (Number.isNaN(number1) === true)
+  number1 = parseInt(prompt('введите n'), 10);
+} while (Number.isNaN(number1))
 
 do {
-  let checkNumber = parseInt(prompt('введите m'),10);
-  number2 = checkNumber;
-} while (Number.isNaN(number2) === true)
+  number2 = parseInt(prompt('введите m'), 10);
+} while (Number.isNaN(number2))
 
-if(number1 > number2) {
+if (number1 > number2) {
   alert('число n должно быть меньше m');
-  do {
-    let checkNumber = parseInt(prompt('введите n'),10);
-    number1 = checkNumber;
-  } while (Number.isNaN(number1) === true)
-  
-  do {
-    let checkNumber = parseInt(prompt('введите m'),10);
-    number2 = checkNumber;
-  } while (Number.isNaN(number2) === true)
+  [number1, number2] = [number2, number1];
 }
 
-const questionDoubleNumbers = confirm('пропускать парные числа?');
+const skipEven = confirm('пропускать парные числа?');
 let sumNumbers = 0;
 
-if (questionDoubleNumbers === false) {
+if (!skipEven) {
   for(let i = number1; i <= number2; i++) {
     sumNumbers += i;  
   }
--- hw_3/main.js
+++ hw_3/main.js
@@ -1,52 +1,39 @@
 //1 Створити функцію getMaxDigit(number) – яка отримує будь-яке число та виводить найбільшу цифру в цьому числі.
-function getMaxDigit(number) {
-  let result = -1;
-  for (let i = number; i > 0; i = Math.floor(i / 10)) {
-    let digit = i % 10;
-    if (result < digit) {
-      result = digit;
-    }
-  }
-  return result;
-}
+// function getMaxDigit (number) {
+//   let result = -1;
+//   for (let i = number; i > 0; i = Math.floor(i / 10)) {
+//     let digit = i % 10;
+//     if (result > digit) {
+//       result = digit;
+//     }
+//   }
+//   return result;
+// }
+
+const getMaxDigit = (number) => +Math.max(...(number + '').split(''));
 
 //2 Створити функцію, яка визначає ступінь числа. Не використовуючи Math.pow та **. Використовуйте цикл.
 function exponentiation (n, exp) {
   if (exp === 0) return 1;
   let result = n;
-  for(let i = 1; i < exp; i++) {
+  for (let i = 1; i < exp; i++) {
     result *= n;
   }
   return result;
 }
 
 //3 Створити функцію, яка форматує ім'я, роблячи першу букву великою. ("влад" -> "Влад", "вЛАД" -> "Влад" і так далі);
-function formatName (str) {
-  let upperLetter;
-  let name = '';
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === str[i].toUpperCase()) {
-      name += str[i].toLowerCase();
-    } else {
-      name += str[i];
-    }
-    upperLetter = name[0].toUpperCase() + name.slice(1);;
-  }
-  return upperLetter;
-
-}
+const formateName = name => name[0].toUpperCase() + name.slice(1).toLowerCase();
 
 //4 Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M. Приклад: getRandomNumber(1, 10) -> 5.
-function getRandomNumber (numberMin, numberMax) {
-  return Math.floor(Math.random() * (numberMax - numberMin + 1) + numberMin);
-}
+const getRandomNumber = (numberMin, numberMax) => Math.floor(Math.random() * (numberMax - numberMin + 1) + numberMin);
 
 //5 Створити функцію, яка рахує скільки разів певна буква повторюється в слові. Приклад: countLetter("а", "Асталавіста") -> 4.
-function countLetter (str,matchLetter) {
+function countLetter (str, matchLetter) {
   let countLetter = 0;
-  for (let i = 0; i < str[i].length; i++) {
-    if(str[i] === matchLetter) {
-      countLetter = str.split(matchLetter).length - 1;
+  for (let i = 0; i <= str.length; i++) {
+    if (str[i] === matchLetter) {
+      countLetter++;
     }
   }
   return countLetter;
@@ -55,7 +42,7 @@ function countLetter (str,matchLetter) {
 //6 Створіть функцію, яка видаляє всі букви з речення. Приклад: deleteLetters('a', "blablabla") -> "blblbl".
 function removeLetter (str, removeLetter) {
   let resultStr = '';
-  for(let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     if (str[i] !== removeLetter) {
       resultStr += str[i];
     }
@@ -63,28 +50,9 @@ function removeLetter (str, removeLetter) {
   return resultStr;
 }
 
-//1
-// const n = +prompt("введите число");
-// console.log('максимальная цифра числа: ', getMaxDigit(289));
-
-//2
-// console.log(exponentiation(2,3));
-
-//3
-// console.log(formatName('вАсИлиЙЙ'));
-
-//4
-// console.log(getRandomNumber(1, 8));
-
-//5
-// console.log(countLetter('aawer', 'a'));
-
-//6
-// console.log(removeLetter('dfgaaafcsdaaaa','a'));
-
-document.writeln('<pre>функция 1: ', getMaxDigit(27509), '</pre>');
-document.writeln('<pre>функция 2:  ', exponentiation(3,3), '</pre>');
-document.writeln('<pre>функция 3: ', formatName('дМитРИй'), '</pre>');
+document.writeln('<pre>функция 1: ', getMaxDigit(908), '</pre>');
+document.writeln('<pre>функция 2:  ', exponentiation(3,2), '</pre>');
+document.writeln('<pre>функция 3: ', formateName('дМитРИй'), '</pre>');
 document.writeln('<pre>функция 4: ', getRandomNumber(1,8), '</pre>');
 document.writeln('<pre>функция 5: ', countLetter('frftyq1', 'f'), '</pre>');
 document.writeln('<pre>функция 6: ', removeLetter('gftrysskkk', 'k'), '</pre>');
