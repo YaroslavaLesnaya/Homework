119c8479a5f49cbeae8dadd0feebed6f373325f5
Yaroslava <yaroslavalesnaya@gmail.com>
2022-08-09 18:36:03 +0300

    refHW_4

--- hw_4/main.js
+++ hw_4/main.js
@@ -2,51 +2,59 @@ const students = ['Олександр', 'Ігор', 'Олена', 'Іра', 'О�
 const themes = ['Диференційне рівняння', 'Теорія автоматів', 'Алгоритми та структури даних'];
 const marks = [4, 5, 5, 3, 4, 5];
 
-// const couples = [[students[0], students[2]], [students[1], students[3]], [students[4], students[5]]]; 
-// const couplesProjects = [[couples[0].join(' i '), themes[0]], 
-//   [couples[1].join(' i '), themes[1]], 
-//   [couples[2].join(' i '), themes[2]]];
-// const studentsMarks = [[students[0], marks[0]], [students[1], marks[1]], [students[2], marks[2]],
-//   [students[3], marks[3]], [students[4], marks[4]], [students[5], marks[5]]];
-// const couplesMarks = [[couplesProjects[0].join(', '), Math.floor((Math.random()*5)+1)],
-//   [couplesProjects[1].join(', '), Math.floor((Math.random()*5)+1)],
-//   [couplesProjects[2].join(', '), Math.floor((Math.random()*5)+1)]];
+//func#1 match students pair 
+const getStudentsCouples = () => {
+  const getStudentsCouples = [];
+  getStudentsCouples.push([students[0], students[2]], [students[1], students[3]], [students[4], students[5]]);
 
-const couples = () => {
-  return [[students[0], students[2]], [students[1], students[3]], [students[4], students[5]]]; 
+  return getStudentsCouples;
 }
 
-const resultCouples = couples();
+const studentsCouples = getStudentsCouples();
 
-const couplesProjects = () => {
-  return [[resultCouples[0].join(' i '), themes[0]], 
-    [resultCouples[1].join(' i '), themes[1]], 
-    [resultCouples[2].join(' i '), themes[2]]];
+//func#2 match students and their themes
+const getCouplesProjects = () => {
+  const getCouplesProjects = [];
+  getCouplesProjects.push(
+    [studentsCouples[0].join(' i '), themes[0]],
+    [studentsCouples[1].join(' i '), themes[1]], 
+    [studentsCouples[2].join(' i '), themes[2]]);
+
+  return getCouplesProjects;
 }
 
-const resultCouplesProjects = couplesProjects();
+const couplesProjects = getCouplesProjects();
 
-const studentsMarks = () => {
-  return [[students[0], marks[0]], [students[1], marks[1]], [students[2], marks[2]],
-    [students[3], marks[3]], [students[4], marks[4]], [students[5], marks[5]]];
+//func#3 match students and their marks
+const getStudentsMarks = () => {
+  const getStudentsMarks = [];
+  getStudentsMarks.push([students[0], marks[0]], [students[1], marks[1]], [students[2], marks[2]],
+    [students[3], marks[3]], [students[4], marks[4]], [students[5], marks[5]]);
+
+  return getStudentsMarks;
 }
 
-const resultStudentsMarks = studentsMarks();
+const studentsMarks = getStudentsMarks();
 
-const couplesMarks = () => {
-  return [[resultCouplesProjects[0].join(', '), Math.floor((Math.random()*5)+1)],
-    [resultCouplesProjects[1].join(', '), Math.floor((Math.random()*5)+1)],
-    [resultCouplesProjects[2].join(', '), Math.floor((Math.random()*5)+1)]];
+//func#4 match students pair and random marks
+const getCouplesMarks = () => {
+  const getCouplesMarks = [];
+  getCouplesMarks.push(
+    [couplesProjects[0].join(', '), Math.floor((Math.random()*5)+1)],
+    [couplesProjects[1].join(', '), Math.floor((Math.random()*5)+1)],
+    [couplesProjects[2].join(', '), Math.floor((Math.random()*5)+1)]);
+
+     return getCouplesMarks;
 }
 
-const resultCouplesMarks = couplesMarks();
+const couplesMarks = getCouplesMarks();
 
-console.log(resultCouples);
-console.log(resultCouplesProjects);
-console.log(resultStudentsMarks);
-console.log(resultCouplesMarks);
+console.log(studentsCouples);
+console.log(couplesProjects);
+console.log(studentsMarks);
+console.log(couplesMarks);
 
-document.writeln('<pre>', resultCouples, '</pre>');
-document.writeln('<pre>', resultCouplesProjects, '</pre>');
-document.writeln('<pre>', resultStudentsMarks, '</pre>');
-document.writeln('<pre>', resultCouplesMarks, '</pre>');
+document.writeln('<pre>', studentsCouples, '</pre>');
+document.writeln('<pre>', couplesProjects, '</pre>');
+document.writeln('<pre>', studentsMarks, '</pre>');
+document.writeln('<pre>', couplesMarks, '</pre>');
