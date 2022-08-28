623526298f01c3e63b28da81ccc0705e2b73974f
Yaroslava <yaroslavalesnaya@gmail.com>
2022-08-28 21:26:09 +0300

    finalRef

--- hw_4/main.js
+++ hw_4/main.js
@@ -5,6 +5,7 @@ const marks = [4, 5, 5, 3, 4, 5];
 //func#1 match students pair 
 const getStudentsCouples = () => {
   const getStudentsCouples = [];
+
   getStudentsCouples.push([students[0], students[2]], [students[1], students[3]], [students[4], students[5]]);
 
   return getStudentsCouples;
@@ -15,6 +16,7 @@ const studentsCouples = getStudentsCouples();
 //func#2 match students and their themes
 const getCouplesProjects = () => {
   const getCouplesProjects = [];
+
   for (let i = 0; i < themes.length; i++) {
     getCouplesProjects.push([studentsCouples[i].join(' i '), themes[i]]);
   }
@@ -27,6 +29,7 @@ const couplesProjects = getCouplesProjects();
 //func#3 match students and their marks
 const getStudentsMarks = () => {
   const getStudentsMarks = [];
+
   for (let i = 0; i < students.length; i++) {
     getStudentsMarks.push([students[i], marks[i]]);
   }
@@ -39,6 +42,7 @@ const studentsMarks = getStudentsMarks();
 //func#4 match students pair and random marks
 const getCouplesMarks = () => {
   const getCouplesMarks = [];
+  
   for (let i = 0; i < couplesProjects.length; i++) {
     getCouplesMarks.push([couplesProjects[i].join(', '), Math.floor((Math.random() * 5) + 1)]);
   }
