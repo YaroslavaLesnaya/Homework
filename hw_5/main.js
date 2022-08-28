dde7454d2a9879eb4c927e08422ce52e8674ac9d
Yaroslava <yaroslavalesnaya@gmail.com>
2022-08-28 21:28:06 +0300

    ref_5.3

--- hw_5/main.js
+++ hw_5/main.js
@@ -21,18 +21,21 @@ console.log('Count positive numbers:', countPositiveNumbers);
 // У функції є параметри: length - довжина масиву, min – мінімальне значення цілого числа,
 // max – максимальне значення цілого числа. Приклад: getRandomArray(15, 1, 100) –>
 // [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2]
+function generationRandomNumber (max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const getRandomArray = (length, min, max) => {
   const randomNumbers = [];
 
-  for (let i = min; i < length + 1; i++) {
-    const randomNumber = Math.round(Math.random() * (max - min + 1));
-    randomNumbers.push(randomNumber);
+  for (let i = 0; i < length; i++) {
+    randomNumbers.push(generationRandomNumber(max, min));
   }
 
   return randomNumbers;
 }
 
-const randomArray = getRandomArray(15, 1, 100);
+const randomArray = getRandomArray(10, 15, 20);
 console.log('Random array:', randomArray);
 
 
@@ -62,9 +65,10 @@ console.log('Average of numbers:', averageNumbers);
 // Приклад: divideByThree("Commander) -> ["com", "man", "der"] Приклад: divideByThree("live") -> ["liv", "e"]
 const getDivideByThree = word => {
   const toOneWord = word.replaceAll(' ', '').split(" ");
+  console.log(toOneWord);
   const dividedByThree = toOneWord.map(function (str) {
-    if (str.length >= 3) {
       const wordByThree = [];
+
       str.split("").forEach(function (str, length) {
         if ((length + 1) % 3 === 0) {
           wordByThree.push(str, " ");
@@ -75,10 +79,10 @@ const getDivideByThree = word => {
 
       return wordByThree.join("");
     }
-  });
+  );
 
   return dividedByThree;
 }
 
-const dividedByThree = getDivideByThree('ertgfku cbhg clsdd');
+const dividedByThree = getDivideByThree('ertku cbhg clsdd cs c');
 console.log('Words divided by 3 letters:', dividedByThree);
