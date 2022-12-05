3604328e185f01e4bedc26b2e79294d23721f088
Yaroslava <yaroslavalesnaya@gmail.com>
2022-12-05 18:07:42 +0200

    ref_9.1

--- hw_9/main.js
+++ hw_9/main.js
@@ -1,14 +1,30 @@
+const body = document.querySelector('body');
+const container = document.createElement('div');
+container.style = " display: none; flex-wrap: wrap; height: 250px; width: 250px;"
+body.append(container);
+
+for (let i = 0; i < 25; i++) {
+  const square = document.createElement('div');
+  square.style = " height: 50px; width: 50px;"
+  square.className = "square";
+  container.append(square);
+}
+
+function randomColor () {
+  const x = parseInt(Math.random() * 256);
+  const y = parseInt(Math.random() * 256);
+  const z = parseInt(Math.random() * 256);
+  const randomColor = "rgb("+ x +", "+ y +", "+ z +")";
+
+  return randomColor;
+}
+
 function generateBlocks() {
-  const showBlocks = document.querySelector('.container');
-  showBlocks.style.display = 'flex';
+  container.style.display = 'flex';
   const backgroundColor = document.querySelectorAll('.square');
 
   for (let i = 0; i < backgroundColor.length; i++) {
-    const x = parseInt(Math.random() * 256);
-    const y = parseInt(Math.random() * 256);
-    const z = parseInt(Math.random() * 256);
-    const randomColor = "rgb("+ x +", "+ y +", "+ z +")";
-    backgroundColor[i].style.background = randomColor;
+    backgroundColor[i].style.background = randomColor();
   }
 }
 // generateBlocks();
@@ -19,4 +35,3 @@ function generateBlocksInterval() {
   }, 1000);
 }
 generateBlocksInterval();
-
